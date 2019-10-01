7d135e408a76429c4849d5141e910622f8c5b5a1
Carlos Tezo <ctezo@hotmail.com>
2019-10-01 15:43:44 -0600

    Se adiciona FrontEnd Para Reporte estadistico
    Se adiciona FrontEnd Para Reporte estadistico

--- angular-8/src/app/material-component/reportes/reportes.component.ts
+++ angular-8/src/app/material-component/reportes/reportes.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { LoginService } from '../../core/services/login.service';
+import { first, flatMap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { RescateService } from '../../core/services/rescate.service.';
 
 @Component({
   selector: 'app-reportes',
@@ -6,6 +12,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./reportes.component.scss']
 })
 export class ReportesComponent {
+  rptEstaMensual: FormGroup;
+  msgrptEstaMensual = 'Imprimir';
+  viewSpinnerImp = false;
+  viewMsg = false;
+  msgRes = '';
+
   typesOfShoes = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
   messages: any[] = [
     {
@@ -52,4 +64,48 @@ export class ReportesComponent {
       updated: new Date('1/18/16')
     }
   ];
+
+  constructor(private fb: FormBuilder, private loginService: LoginService, private ToastrService: ToastrService, private rescateService: RescateService) {
+    this.rptEstaMensual = this.fb.group({
+      Cod_Compania: [loginService.currentUserValue.cod_compania[0], [Validators.required]],
+      Anio: ['', [Validators.required]],
+      Mes: ['', [Validators.required]],
+      Cod_TipoReporte: ['1']
+    });
+  }
+
+  submitrptEstaMensual() {
+
+    // stop here if form is invalid
+    if (this.rptEstaMensual.invalid) {
+      this.viewMsg = false;
+      return;
+    }
+
+    this.msgrptEstaMensual = 'Descargando...';
+    this.viewSpinnerImp = true;
+    this.viewMsg = false;
+    this.rescateService.ImpresionRptEstaMensual(this.rptEstaMensual.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.viewSpinnerImp = false;
+          this.msgrptEstaMensual = "Imprimir"
+          const linkSource = 'data:application/pdf;base64,' + data.msgRespuesta;
+          const downloadLink = document.createElement("a");
+          const fileName = this.rptEstaMensual.controls.Cod_Compania.value + "_" + this.rptEstaMensual.controls.Anio.value + "_" + this.rptEstaMensual.controls.Mes.value + ".pdf";
+          downloadLink.href = linkSource;
+          downloadLink.download = fileName;
+          downloadLink.click();
+        },
+        (error: HttpErrorResponse) => {
+          this.viewSpinnerImp = false;
+          this.msgrptEstaMensual = "Imprimir"
+          this.viewMsg = true;
+          this.ToastrService.error(error.error.msgRespuesta == "undefined" ? "Ocurrió un error por favor intentarlo de nuevo o más tarde." : error.error.msgRespuesta, "Error en operación", {
+            progressBar: true
+          });
+        });
+
+  }
 }
